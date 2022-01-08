ef8d2a3f374c77758cc1650e3dcb3d95a753a96e
gopinath <gopinath.vempati@xcubelabs.com>
2022-01-08 20:03:19 +0530

    added new files

--- src/BaseClass.java
+++ src/BaseClass.java
@@ -1,2 +1,49 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class BaseClass {
+
+    static ArrayList<Receipe> getRecepiesList(ArrayList<Receipe> receipe) throws IOException {
+        File reciepe = new File("/home/gopinath/Desktop/PrepForTech/RestraurantManagementSystem/src/TextFiles/receipe.txt");
+        BufferedReader br = new BufferedReader(new FileReader(reciepe));
+        String st;
+        while ((st = br.readLine()) != null) {
+            ArrayList<Ingredients> ingredients = new ArrayList<>();
+            String[] values = st.split(" ");
+            if(values.length>0) {
+                for (int i = 1; i <= values.length - 2; i++) {
+                    if (i % 2 == 1) {
+                        ingredients.add(new Ingredients(values[i], Double.parseDouble(values[i + 1])));
+                    }
+                }
+                receipe.add(new Receipe(values[0], ingredients));
+            }
+        }
+        return receipe;
+    }
+
+    static Accounts getAccountInfo() throws IOException {
+        File account = new File("/home/gopinath/Desktop/PrepForTech/RestraurantManagementSystem/src/TextFiles/accounts.txt");
+        BufferedReader br = new BufferedReader(new FileReader(account));
+        String st;
+        Accounts accounts = null;
+        while ((st = br.readLine()) != null) {
+            accounts = new Accounts(Long.parseLong(st));
+        }
+        return accounts;
+    }
+
+    static void getInventoriesList(ArrayList<Inventory> inventories) throws IOException {
+        File ingredientTxtFile = new File("/home/gopinath/Desktop/PrepForTech/RestraurantManagementSystem/src/TextFiles/inventory.txt");
+        BufferedReader br = new BufferedReader(new FileReader(ingredientTxtFile));
+        String st;
+        while ((st = br.readLine()) != null) {
+            String[] arr = st.split(" ");
+            Inventory ingredients1 = new Inventory(arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
+            inventories.add(ingredients1);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends BaseClass{
     public static void main(String[] args) throws IOException {
         /*Holds the list of invetory present in restrauant*/
         ArrayList<Inventory> inventories = new ArrayList<>();
@@ -11,61 +11,23 @@ public class Main {
         /*Holds details of each receipe*/
         ArrayList<Receipe> receipe = new ArrayList<>();
 
-
         getInventoriesList(inventories);
         accounts = getAccountInfo();
         System.out.println(accounts.getMoney());
+
         getRecepiesList(receipe);
         for (Receipe r : receipe){
             for (Ingredients ingredients : r.getIngredients())
                 System.out.println(r.getName()+" "+ingredients.getIngredientName()+" "+ingredients.getQuantity());
         }
+
         Scanner scanner = new Scanner(System.in);
-        // System.out.println("Enter a command : ");
+        System.out.println("Enter a command : ");
         int command = scanner.nextInt();
         switch (command) {
             case 0 -> {
 
             }
-        }
-    }
-
-    private static ArrayList<Receipe> getRecepiesList(ArrayList<Receipe> receipe) throws IOException {
-        File reciepe = new File("/home/gopinath/Desktop/PrepForTech/RestraurantManagementSystem/src/TextFiles/receipe.txt");
-        BufferedReader br = new BufferedReader(new FileReader(reciepe));
-        String st;
-        while ((st = br.readLine()) != null) {
-            ArrayList<Ingredients> ingredients = new ArrayList<>();
-            String[] values = st.split(" ");
-            for (int i = 1; i <= values.length - 2; i++) {
-                if (i % 2 == 1) {
-                    ingredients.add(new Ingredients(values[i], Double.parseDouble(values[i + 1])));
-                }
-            }
-            receipe.add(new Receipe(values[0], ingredients));
-        }
-        return receipe;
-    }
-
-    private static Accounts getAccountInfo() throws IOException {
-        File account = new File("/home/gopinath/Desktop/PrepForTech/RestraurantManagementSystem/src/TextFiles/accounts.txt");
-        BufferedReader br = new BufferedReader(new FileReader(account));
-        String st;
-        Accounts accounts = null;
-        while ((st = br.readLine()) != null) {
-            accounts = new Accounts(Long.parseLong(st));
-        }
-        return accounts;
-    }
-
-    private static void getInventoriesList(ArrayList<Inventory> inventories) throws IOException {
-        File ingredientTxtFile = new File("/home/gopinath/Desktop/PrepForTech/RestraurantManagementSystem/src/TextFiles/inventory.txt");
-        BufferedReader br = new BufferedReader(new FileReader(ingredientTxtFile));
-        String st;
-        while ((st = br.readLine()) != null) {
-            String[] arr = st.split(" ");
-            Inventory ingredients1 = new Inventory(arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
-            inventories.add(ingredients1);
         }
     }
 }
